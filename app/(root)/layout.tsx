82c52337f9828a167d28c03483efdc8ce8788c79
AxelothLeohryn <alejandro97ss@gmail.com>
2024-03-30 18:15:18 +0100

    add navbar and sidebar

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,8 @@
+import React, { ReactNode } from "react";
+
+const RootLayout = ({ children }: { children: ReactNode }) => {
+  return <main>
+    {children}</main>;
+};
+
+export default RootLayout;
---
+++ components/Navbar.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import MobileNav from "./MobileNav";
+
+const Navbar = () => {
+  return (
+    <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:p">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="icons/logo.svg"
+          width={32}
+          height={32}
+          alt="Zoom logo"
+          className="max-sm:size-10"
+        />
+        <p className="text-[26px] font-extrabold text-white max-sm:hidden">
+          Zoom
+        </p>
+      </Link>
+      <div className="flex-between gap-5">
+        {/* Clerk - User Management */}
+
+        <MobileNav />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
