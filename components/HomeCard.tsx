72738da6fc8c397f808d0feae20eadc33d645f46
AxelothLeohryn <alejandro97ss@gmail.com>
2024-03-30 21:34:46 +0100

    add reusable modal

--- components/HomeCard.tsx
+++ components/HomeCard.tsx
@@ -23,7 +23,7 @@ const HomeCard = ({
         "px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px] cursor-pointer",
         className
       )}
-      onClick={() => {}}
+      onClick={handleClick}
     >
       <div className="flex-center glassmorphism size-12 rounded-[10px]">
         <Image src={img} alt="Meeting" width={27} height={27} />
--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import Image from "next/image";
 import React, { useState } from "react";
-import HomeCard from "./HomeCard";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
+import HomeCard from "./HomeCard";
+import MeetingModal from "./MeetingModal";
 
 const MeetingTypeList = () => {
   const router = useRouter();
@@ -11,13 +12,17 @@ const MeetingTypeList = () => {
     "isScheduleMeeting" | "isJoiningMeeting" | "isInstantMeeting" | undefined
   >();
 
+  const createMeeting = () => {
+    
+  }
+
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
       <HomeCard
         img="/icons/add-meeting.svg"
         title="New Meeting"
         description="Star an instant meeting"
-        handleClick={() => setMeetingState("isJoiningMeeting")}
+        handleClick={() => setMeetingState("isInstantMeeting")}
         className="bg-orange-1"
       />
       <HomeCard
@@ -40,6 +45,15 @@ const MeetingTypeList = () => {
         description="Via invitation link"
         handleClick={() => setMeetingState("isJoiningMeeting")}
         className="bg-yellow-1"
+      />
+
+      <MeetingModal 
+        isOpen={meetingState === "isInstantMeeting"}
+        onClose={() => setMeetingState(undefined)}
+        title="Start an Instant Meeting"
+        className="text-center"
+        buttonText="Start Meeting"
+        handleClick={createMeeting}
       />
     </section>
   );
